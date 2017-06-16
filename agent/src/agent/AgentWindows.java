210c72efcd403f621784a69b23b54b0a063a58ab
Novica Nikolic <nole0223@gmail.com>
2017-06-16 17:56:29 +0200

    Agent_Siem fix problem of read login file

---
+++ agent/src/agent/AgentWindows.java
@@ -0,0 +1,109 @@
+package agent;
+
+import java.util.Date;
+
+import org.json.simple.JSONObject;
+
+import com.sun.jna.platform.win32.Advapi32Util.EventLogIterator;
+import com.sun.jna.platform.win32.Advapi32Util.EventLogRecord;
+
+public class AgentWindows {
+	
+	//Konfiguracije
+	private long time;
+	private String password;
+	private String ip_address;
+	private String server_port;
+	private String ID;
+	private String type;
+	private String username;
+	
+	//OVO SE SALJE NA SERVER SVE + type iz konfiguracije
+	//KOJI NAM GOVORI DA LI JE LINUX WINDOWS ... RADI LAKSEG CUVANJA
+	//A PODACI O AGENTU CE BITI U USER_LOGIN KADA SE LOGUJE
+	//TAKO DA PAZI I NJIH DA ISKORISTI
+	private String recordNumber;
+	private String logType;
+	private String timeLog;
+	private String sourceLog;
+	private String computerName;
+	private String messages;
+	
+	public AgentWindows(JSONObject config) {
+		super();
+		
+		time = (long) config.get("");
+		password = (String) config.get("password");
+		ip_address = (String) config.get("ip_address");
+		server_port = (String) config.get("server_port");
+		ID = (String) config.get("ID");
+		type = (String) config.get("type");
+		username = (String) config.get("username");
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+			}
+		});
+	}
+	
+	public void run() {
+		boolean start = true;
+		
+		String sourceName = "System";
+		while(start) {
+			try{
+				
+				EventLogIterator log = new EventLogIterator(sourceName);
+				while(log.hasNext()) {
+					EventLogRecord rec = log.next();
+					
+					Date toDay = new Date();
+					Date logDate = new Date();
+					
+					int dayToDay = toDay.getDate();
+					int hoursToDay = toDay.getHours();
+					
+					logDate.setTime(rec.getRecord().TimeWritten.intValue()*1000L);
+					
+					int logToDay = logDate.getDate();
+					int logHoursDay = logDate.getHours();
+					
+					recordNumber = String.valueOf(rec.getRecordNumber());
+					logType = rec.getType().toString();
+					timeLog = logDate.toString();
+					sourceLog = rec.getSource();
+					
+					if(dayToDay == logToDay) {
+						if((hoursToDay-1) <= logHoursDay) {
+							
+							
+							String result = jWMI.getWMIValue("Select * from Win32_NTLogEvent where LogFile='"+sourceName+"' and RecordNumber="+rec.getRecordNumber(), "ComputerName, Message");
+							
+							int computerId = result.indexOf("\r\n");
+							
+							if(computerId != -1) {
+								
+								computerName = result.substring(0, computerId);
+								messages = result.substring(computerId+2, result.length());
+							} else {
+								
+								computerName = result;
+								messages = "";
+							}
+							
+							//Ovo je struktura koja je iscitana iz logFajla
+							String sendLog = recordNumber + " " + logType + " " + timeLog 
+									+ " " + sourceLog + " " + computerName + " " + messages + " " + type;
+							
+							System.out.println(sendLog);
+						}
+					}
+				}
+				Thread.sleep(time);
+			} catch (Exception e) {
+				e.printStackTrace();
+				start = false;
+			}
+		}
+	}
+}
--- agent/src/agent/Main.java
+++ agent/src/agent/Main.java
@@ -1,24 +1,51 @@
 package agent;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 
 public class Main {
 
 	static Scanner scanner;
 	static String filePath = "configs.json";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
 
 		scanner = new Scanner(System.in);
 		File file = new File(filePath);
 
 		if (!file.exists())
 			setUp();
+		
+		JSONParser pareser = new JSONParser();
+		
+		JSONObject config = (JSONObject) pareser.parse(new FileReader(filePath));
+		
+		
+		if(config.get("type").equals("windows")) {
+			
+			//TREBA POVEZATI SA AgentWindows
+			System.out.println("Ovde ide AgentWindows");
+			
+		} else if (config.get("type").equals("linux")){
+			
+			//TREBA POVEZATI SA Linux
+			System.out.println("Ovde ide Linux sistem");
+			
+		} else if(config.get("type").equals("firewal")) {
+			
+			//OVDE IDE FIREVAL
+			System.out.println("Ovde ide firewal");
+		}
 	}
 
 	public static void setUp() {
@@ -26,8 +53,8 @@ public class Main {
 		String fileInput = null;
 		JSONObject config = new JSONObject();
 		String[] printStrings = { "Enter agent ID", "Please enter username", "Please enter password",
-				"Please enter center Ip address", "Please enter server port", "Please enter path to log file" };
-		String[] configFields = { "ID", "username", "password", "Ip_address", "server_port", "log_path" };
+				"Please enter center Ip address", "Please enter server port", "Please enter type system", "Pleas enter time of sleep" };
+		String[] configFields = { "ID", "username", "password", "Ip_address", "server_port", "type", "sleep" };
 
 		for (int i = 0; i < printStrings.length; i++) {
 
